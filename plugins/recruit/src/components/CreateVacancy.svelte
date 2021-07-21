e5289a76652b960624df7a16a2acd7f8c25d2125
Andrey Platov <andrey@hardcoreeng.com>
2021-07-21 18:30:25 +0200

    update `CreateVacancy`
    Signed-off-by: Andrey Platov <andrey@hardcoreeng.com>

--- plugins/recruit/src/components/CreateVacancy.svelte
+++ plugins/recruit/src/components/CreateVacancy.svelte
@@ -15,9 +15,10 @@
 
 <script lang="ts">
   import { createEventDispatcher } from 'svelte'
-  import { TextArea, EditBox, Dialog, ToggleWithLabel } from '@anticrm/ui'
+  import { TextArea, EditBox, Dialog, ToggleWithLabel, Tabs, Section } from '@anticrm/ui'
 
   import { getClient } from '@anticrm/workbench'
+  import File from './icons/File.svelte'
 
   import recruit from '../plugin'
   import core from '@anticrm/core'
@@ -43,24 +44,35 @@
         okLabel={recruit.string.CreateVacancy} 
         okAction={createVacancy}
         on:close={() => { dispatch('close') }}>
+  <Tabs/>
   <div class="content">
-    <div class="row"><EditBox label={recruit.string.VacancyName} bind:value={name}/></div>
-    <div class="row"><TextArea label={recruit.string.VacancyDescription} bind:value={description}/></div>
-    <div class="row"><ToggleWithLabel label={recruit.string.MakePrivate} description={recruit.string.MakePrivateDescription}/></div>
+    <Section icon={File} label={'Personal Information'}>
+      <div class="grid">
+        <div class="row"><EditBox label={recruit.string.VacancyName} bind:value={name} placeholder="Software Engineer"/></div>
+        <div class="row"><TextArea label={recruit.string.VacancyDescription} bind:value={description}/></div>
+        <div class="row"><ToggleWithLabel label={recruit.string.MakePrivate} description={recruit.string.MakePrivateDescription}/></div>
+      </div>
+    </Section>
   </div>
 </Dialog>
 
 
 <style lang="scss">
 
-  .content {
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    row-gap: 20px;
-    
-    .row {
-      grid-column-start: 1;
-      grid-column-end: 3;
+.content {
+    display: flex;
+    flex-direction: column;
+
+    .grid {
+      display: grid;
+      grid-template-columns: 1fr 1fr;
+      column-gap: 24px;
+      row-gap: 40px;
+      
+      .row {
+        grid-column-start: 1;
+        grid-column-end: 3;
+      }
     }
   }
 
