5caacc9c5eb931dcb17fc7b08db7fe56cf5a907c
Alexander Platov <sas_lord@mail.ru>
2021-07-21 23:32:53 +0300

    Fix UserBox (#9)
    Signed-off-by: Alexander Platov <sas_lord@mail.ru>

--- packages/ui/src/components/UserBox.svelte
+++ packages/ui/src/components/UserBox.svelte
@@ -35,7 +35,7 @@
                                { name: 'elon', title: 'Elon Musk' },
                                { name: 'kathryn', title: 'Kathryn Minshew' }]
   export let vAlign: 'top' | 'middle' | 'bottom' = 'bottom'
-  export let hAlign: 'left' | 'center' | 'right' = 'left'
+  export let hAlign: 'left' | 'center' | 'right' = 'right'
   export let margin: number = 16
   export let showSearch: boolean = false
 
@@ -57,7 +57,7 @@
       {#if selected}
         <div class="avatar"><UserInfo user={selected.name} size={36} avatarOnly /></div>
       {:else}
-        <div class="icon">{#if pressed}<Close/>{:else}<Add/>{/if}</div>
+        <div class="icon">{#if pressed}<Close size={16} />{:else}<Add size={16} />{/if}</div>
       {/if}
     </button>
     {#if selected}
