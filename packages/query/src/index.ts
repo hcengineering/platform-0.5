0a1a3f0d8aaae6f560248665b7c8252a8ca276f6
Andrey Platov <andrey@hardcoreeng.com>
2021-07-24 17:52:46 +0200

    query lookup
    Signed-off-by: Andrey Platov <andrey@hardcoreeng.com>

--- packages/query/src/index.ts
+++ packages/query/src/index.ts
@@ -13,7 +13,9 @@
 // limitations under the License.
 //
 
-import { Ref, Class, Doc, Tx, DocumentQuery, TxCreateDoc, TxRemoveDoc, Client, Obj, FindOptions, TxUpdateDoc, getOperator, TxProcessor, resultSort, SortingQuery, FindResult, Hierarchy } from '@anticrm/core'
+import { Ref, Class, Doc, Tx, DocumentQuery, TxCreateDoc, TxRemoveDoc, Client, 
+  Obj, FindOptions, TxUpdateDoc, getOperator, TxProcessor, resultSort, SortingQuery, 
+  FindResult, Hierarchy, Refs, WithLookup, LookupData } from '@anticrm/core'
 
 interface Query {
   _class: Ref<Class<Doc>>
@@ -91,14 +93,26 @@ export class LiveQuery extends TxProcessor implements Client {
     }
   }
 
+  private async lookup(doc: Doc, lookup: Refs<Doc>) {
+    const result: LookupData<Doc> = {}
+    for (const key in lookup) {
+      const _class = (lookup as any)[key] as Ref<Class<Doc>>
+      const _id = (doc as any)[key] as Ref<Doc>
+      (result as any)[key] = (await this.client.findAll(_class, { _id }))[0]
+    }
+    (doc as WithLookup<Doc>).$lookup = result
+  }
+
   async txCreateDoc (tx: TxCreateDoc<Doc>): Promise<void> {
     for (const q of this.queries) {
       const doc = TxProcessor.createDoc2Doc(tx)
       if (this.match(q, doc)) {
-        console.log('matched query')
         if (q.result instanceof Promise) {
           q.result = await q.result
         }
+
+        if (q.options?.lookup !== undefined) this.lookup(doc, q.options.lookup)
+
         q.result.push(doc)
 
         if (q.options?.sort !== undefined) resultSort(q.result, q.options?.sort)
