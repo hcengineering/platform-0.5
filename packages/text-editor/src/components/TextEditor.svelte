da44d4a4fe08ac9cf78850b56e1ad0a3ce061e44
Andrey Platov <andrey@hardcoreeng.com>
2021-07-26 13:41:11 +0200

    add custom javascript node
    Signed-off-by: Andrey Platov <andrey@hardcoreeng.com>

--- packages/text-editor/src/components/TextEditor.svelte
+++ packages/text-editor/src/components/TextEditor.svelte
@@ -27,6 +27,56 @@ import Mention from '@tiptap/extension-mention'
 import MentionList from './MentionList.svelte'
 import { SvelteRenderer } from './SvelteRenderer'
 
+const MyMention = Mention.extend({
+  addNodeView() {
+    return ({ editor, node, getPos }) => {
+      const { view } = editor
+
+      // Markup
+      /*
+        <div class="node-view">
+          <span class="label">Node view</span>
+
+          <div class="content">
+            <button>
+              This button has been clicked ${node.attrs.count} times.
+            </button>
+          </div>
+        </div>
+      */
+
+      const dom = document.createElement('div')
+      dom.classList.add('node-view')
+
+      const label = document.createElement('span')
+      label.classList.add('label')
+      label.innerHTML = 'Node view'
+
+      const content = document.createElement('div')
+      content.classList.add('content')
+
+      const button = document.createElement('button')
+      button.innerHTML = `This button has been clicked ${node.attrs.count} times.`
+      button.addEventListener('click', () => {
+        if (typeof getPos === 'function') {
+          view.dispatch(view.state.tr.setNodeMarkup(getPos(), undefined, {
+            count: node.attrs.count + 1,
+          }))
+
+          editor.commands.focus()
+        }
+      })
+      content.append(button)
+
+      dom.append(label, content)
+
+      return {
+        dom,
+      }
+    }
+  }
+})
+
 let element: HTMLElement
 let editor: Editor
 
@@ -38,7 +88,7 @@ onMount(() => {
       Highlight,
       Typography,
       Placeholder.configure({placeholder: 'Type something...'}),
-      Mention.configure({
+      MyMention.configure({
         HTMLAttributes: {
           class: 'mention',
         },
