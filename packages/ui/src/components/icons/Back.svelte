9e6f0545dffe83fc0417eaa84a8c1a7578f4da4a
Alexander Platov <sas_lord@mail.ru>
2021-07-27 20:48:14 +0300

    Fix DataPicker
    Signed-off-by: Alexander Platov <sas_lord@mail.ru>

--- packages/ui/src/components/icons/Back.svelte
+++ packages/ui/src/components/icons/Back.svelte
@@ -1,10 +1,8 @@
 <script lang="ts">
-  export let size: number = 24
+  export let size: number = 16
   const fill: string = 'var(--theme-caption-color)'
 </script>
 
-<svg width={size} height={size} {fill} viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-  <path
-    d="M15.9,21.1c-0.2,0-0.4-0.1-0.5-0.2L7,12.5c-0.3-0.3-0.3-0.8,0-1.1L15.4,3c0.3-0.3,0.8-0.3,1.1,0s0.3,0.8,0,1.1 l-7.9,7.9l7.9,7.9c0.3,0.3,0.3,0.8,0,1.1C16.3,21,16.1,21.1,15.9,21.1z"
-  />
+<svg width={size} height={size} {fill} viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
+	<polygon points="10.6,14.4 4.3,8 10.6,1.6 11.4,2.4 5.7,8 11.4,13.6 "/>
 </svg>
--- packages/ui/src/components/icons/Forward.svelte
+++ packages/ui/src/components/icons/Forward.svelte
@@ -1,10 +1,8 @@
 <script lang="ts">
-  export let size: number = 24
+  export let size: number = 16
   const fill: string = 'var(--theme-caption-color)'
 </script>
 
-<svg width={size} height={size} {fill} viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-  <path
-    d="M8.1,21.2c-0.2,0-0.4-0.1-0.5-0.2c-0.3-0.3-0.3-0.8,0-1.1l7.9-7.9L7.5,4.2c-0.3-0.3-0.3-0.8,0-1.1 s0.8-0.3,1.1,0l8.4,8.4c0.3,0.3,0.3,0.8,0,1.1L8.6,21C8.5,21.2,8.3,21.2,8.1,21.2z"
-  />
+<svg width={size} height={size} {fill} viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
+	<polygon class="st0" points="6.4,14.4 5.6,13.6 11.3,8 5.6,2.4 6.4,1.6 12.7,8 "/>
 </svg>
